a4eaa9604cc4f058ee2e2d9ccee915a9f695ac56
Annanda Sousa <annanda.sousa@gmail.com>
2023-09-05 13:27:40 +0100

    Starting to implement pipeline functions.

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # XR2Learn CLI (Command Line Interface)
 
-A Command Line Interface to use XR2Learn training components. 
-
+A Command Line Interface to use XR2Learn training and inference (not implemented) components.
 
 [Diagram with Architecture Overview](https://drive.google.com/file/d/1k3yLi9Y8tasFMJFNxIwKY-nRJzPdKPLw/view?usp=sharing)
 
-### Installing 
+### Installing
 
 **Dependencies**:
+
 - Docker (or Nvidia-Docker for CUDA)
 - Python 3.10
 
-
 ### Quick-Start
+
 `python train.py <args-list>`
 
-`python train.py --help` for a list of arguments and their description. 
+`python train.py --help` for a list of arguments and their description.
 
 ### Examples
--- train.py
+++ train.py
@@ -11,5 +11,20 @@ def hello(count, name):
         click.echo(f"Hello {name}!")
 
 
+def call_docker(docker_service_name, arguments):
+    pass
+
+
+@click.command()
+@click.option('--dataset', default='RAVDESS', help='Dataset supported by XR2Learn Enablers')
+@click.option('--pre_train', default=True, help='Indicates if SSL is part of the training pipeline')
+@click.option('--fine_tune', default=False, help='Indicates if Supervised Learning is part of the training pipeline')
+def pipeline(dataset, pre_train, fine_tune):
+    """Pipeline entry point"""
+    click.echo(dataset)
+    click.echo(pre_train)
+    click.echo(fine_tune)
+
+
 if __name__ == '__main__':
-    hello()
+    pipeline()
