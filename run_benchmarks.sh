d6cd10d0a21d8a46d3411b6e9c91c8ecd0ff0d6d
Annanda Sousa <annanda.sousa@gmail.com>
2023-11-01 10:00:38 +0100

    Realising the benchmarks are not including CLI.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Actually this benchmarks are not the best because they are not testing CLI as well.
+# This need to be changed: refactor the benchmarks to run them through CLI
+
 ./benchmarks/full_pipeline_standardize.sh
 ./benchmarks/full_pipeline_eGeMAPs.sh
