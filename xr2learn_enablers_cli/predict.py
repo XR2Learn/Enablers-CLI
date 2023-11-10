3703b32284f9bee36f95818f65a2ed38b7c30202
Annanda Sousa <annanda.sousa@gmail.com>
2023-11-10 15:58:07 +0100

    Implementing support for Inference.

--- xr2learn_enablers_cli/predict.py
+++ xr2learn_enablers_cli/predict.py
@@ -2,7 +2,7 @@ from xr2learn_enablers_cli.call_docker import prepare_env_vars, call_docker
 from xr2learn_enablers_cli.conf import SUPPORTED_DATASETS
 
 
-def inference_pipeline(modality, ssl_pre_train, ed_training, features_type, dataset, docker_env_vars):
+def inference_pipeline(modality, dataset, docker_env_vars):
     print(docker_env_vars)
     env_vars = prepare_env_vars(docker_env_vars)
 
@@ -12,6 +12,8 @@ def inference_pipeline(modality, ssl_pre_train, ed_training, features_type, data
 
     if call_docker(f'emotion-classification-{modality}', env_vars=env_vars):
         pass
+
+    return
 
     if call_docker(f'fusion-layer', env_vars=env_vars):
         pass
