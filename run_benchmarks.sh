4adac78bb9f745c4067ca66440dcf126039498bb
Annanda Sousa <annanda.sousa@gmail.com>
2023-11-13 14:36:30 +0100

    Including benchmarks for Inference.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -3,5 +3,8 @@
 
 sudo rm -R outputs/*
 ./benchmarks/full_pipeline_eGeMAPs.sh
+./benchmarks/predict_ravdess_eGeMAPs.sh
+./benchmarks/multimodal_ravdess_eGeMAPs.sh
+./benchmarks/evaluate_ravdess_eGeMAPs.sh
 #sudo rm -R outputs/*
 #./benchmarks/full_pipeline_standardize.sh
