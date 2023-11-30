beaec661d2a8431c50fe4f9e939de5f08834db69
Annanda Sousa <annanda.sousa@gmail.com>
2023-11-30 15:11:38 +0100

    Starting to create tests for CLI.

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,7 @@
 import unittest
 
-
-# from unittest.mock import patch
-#
-# from train import pipeline
+from unittest.mock import patch
+from xr2learn_enablers_cli.train import ssl_pipeline
 
 
 class CliTrainTestCase(unittest.TestCase):
@@ -12,3 +10,24 @@ class CliTrainTestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    # @patch('xr2learn_enablers_cli.train.call_docker')
+    # def test_ssl_pipeline_call_docker_ssl_training_with_correct_parameters_if_not_fe_only(self, mock_call_docker):
+    #     gpu = False
+    #     env_vars = {'env': 'value'}
+    #     modality = 'audio'
+    #     ssl_pre_train = None
+    #     ssl_pipeline(ssl_pre_train, modality, env_vars, gpu)
+    #     mock_call_docker.assert_called_with(ssl_pre_train, modality, env_vars, gpu)
+
+    @patch('xr2learn_enablers_cli.train.call_docker')
+    def test_ssl_pipeline_call_docker_fe_with_correct_parameters_if_not_encoder_only(self, mock_call_docker):
+        # The logic is likely wrong, revise it later.
+        gpu = False
+        env_vars = {'env': 'value'}
+        modality = 'audio'
+        ssl_pre_train = None
+        ssl_pipeline(ssl_pre_train, modality, env_vars, gpu)
+        expected_docker_img_name = f'ssl-features-generation-{modality}'
+        mock_call_docker.assert_called_with(expected_docker_img_name, env_vars=env_vars, gpu=gpu)
+        self.assertEqual(mock_call_docker.call_count, 2)
