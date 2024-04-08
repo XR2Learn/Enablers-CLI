bd6190061be4f532e0a48dacfb1e9430663ab93a
Annanda Sousa <annanda.sousa@gmail.com>
2024-04-08 14:52:39 +0200

    Changing benchmarks to only delete files in the beginning of script.

--- run_benchmarks_bm.sh
+++ run_benchmarks_bm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 [ -e ./logs ] && rm -r ./logs/bm_full_pipeline*
 
-sudo rm -R outputs/XRoom
+sudo rm -R outputs/XRoom/shimmer
+
 ./benchmarks/full_pipeline_bm.sh
 ./benchmarks/predict_bm.sh
 ./benchmarks/multimodal_bm.sh
 ./benchmarks/evaluate_bm.sh
 
-sudo rm -R outputs/XRoom
+#sudo rm -R outputs/XRoom
 ./benchmarks/full_pipeline_bm_end2end.sh
 ./benchmarks/predict_bm_end2end.sh
 ./benchmarks/multimodal_bm_end2end.sh
