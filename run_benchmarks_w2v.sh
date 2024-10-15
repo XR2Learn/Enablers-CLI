857356a6cb9df96a713a22c2d465201d5f5aeb07
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-15 11:02:34 +0200

    Removing evaluate benchmarks - service not included in Inference Tools.

--- run_benchmarks_w2v.sh
+++ run_benchmarks_w2v.sh
@@ -6,4 +6,3 @@ sudo rm -R outputs/RAVDESS
 ./benchmarks/full_pipeline_w2v_finetuning_end2end.sh
 ./benchmarks/predict_ravdess_w2v_finetuning_end2end.sh
 ./benchmarks/multimodal_ravdess_w2v_finetuning_end2end.sh
-./benchmarks/evaluate_ravdess_w2v_finetuning_end2end.sh
