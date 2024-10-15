a851a313b64296fe7cd2a7e66ec0732918dc9bd0
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-15 11:01:48 +0200

    Removing evaluate benchmarks - service not included in Inference Tools.

--- run_benchmarks_audio.sh
+++ run_benchmarks_audio.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 [ -e ./logs ] && rm -r ./logs/egemaps_full_pipeline* ./logs/standardize_full_pipeline*
 
+# Only modality / dataset that still supports non pub/sub Inference.
 sudo rm -R outputs/RAVDESS
 
 ./benchmarks/full_pipeline_eGeMAPs.sh
 ./benchmarks/predict_ravdess_eGeMAPs.sh
 ./benchmarks/multimodal_ravdess_eGeMAPs.sh
-./benchmarks/evaluate_ravdess_eGeMAPs.sh
 
 
 ./benchmarks/full_pipeline_eGeMAPs_end2end.sh
 ./benchmarks/predict_ravdess_eGeMAPs_end2end.sh
 ./benchmarks/multimodal_ravdess_eGeMAPs_end2end.sh
-./benchmarks/evaluate_ravdess_eGeMAPs_end2end.sh
 
 
 ./benchmarks/full_pipeline_standardize.sh
 ./benchmarks/predict_ravdess_standardize.sh
 ./benchmarks/multimodal_ravdess_standardize.sh
-./benchmarks/evaluate_ravdess_standardize.sh
 
 
 ./benchmarks/full_pipeline_standardize_end2end.sh
 ./benchmarks/predict_ravdess_standardize_end2end.sh
 ./benchmarks/multimodal_ravdess_standardize_end2end.sh
-./benchmarks/evaluate_ravdess_standardize_end2end.sh
