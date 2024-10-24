c2591c484afd049184e6e1c864023602ed66584a
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-24 13:11:38 +0200

    Updating audio benchmarks to not include multimodal call.

--- run_benchmarks_audio.sh
+++ run_benchmarks_audio.sh
@@ -6,19 +6,12 @@ sudo rm -R outputs/RAVDESS
 
 ./benchmarks/full_pipeline_eGeMAPs.sh
 ./benchmarks/predict_ravdess_eGeMAPs.sh
-./benchmarks/multimodal_ravdess_eGeMAPs.sh
-
 
 ./benchmarks/full_pipeline_eGeMAPs_end2end.sh
 ./benchmarks/predict_ravdess_eGeMAPs_end2end.sh
-./benchmarks/multimodal_ravdess_eGeMAPs_end2end.sh
-
 
 ./benchmarks/full_pipeline_standardize.sh
 ./benchmarks/predict_ravdess_standardize.sh
-./benchmarks/multimodal_ravdess_standardize.sh
-
 
 ./benchmarks/full_pipeline_standardize_end2end.sh
 ./benchmarks/predict_ravdess_standardize_end2end.sh
-./benchmarks/multimodal_ravdess_standardize_end2end.sh
