8a219dce150a0e333b99fb3b3d7976379e6e58b3
Annanda Sousa <annanda.sousa@gmail.com>
2024-10-25 11:53:13 +0200

    Including body-tracking preprocessing service for training pipeline. Adding benchmarks for body-tracking.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - support for `body-tracking` modality
 - support for multimodality for XRoom dataset (body tracking + bio-measurements) modalities.
+- training benchmarks for body-tracking and bio-measurements
 
 ### Changed 
 - update services names
--- benchmarks/train_pipeline_body-tracking.sh
+++ benchmarks/train_pipeline_body-tracking.sh
@@ -3,9 +3,9 @@
 echo "--------------------"
 echo "TRAIN PIPELINE WITH BODY TRACKING"
 echo "--------------------"
-FILE_NAME=configuration_bm.json
+FILE_NAME=configuration_bm_body-tracking.json
 PATH_JSON="./benchmarks/benchmarks_configs/$FILE_NAME"
-EXPERIMENT_ID=bm_train_pipeline
+EXPERIMENT_ID=body_tracking_train_pipeline
 
 if [ -z "$GPU" ]
 then
@@ -14,8 +14,6 @@ fi
 
 # Dockers called during this use-case:
 # Pre-processing
-# Handcrafted features
-# SSL training
 # Sup training
 
-python xr2learn_enablers_cli/xr2learn_enablers.py --experiment_id $EXPERIMENT_ID --config_file $PATH_JSON --gpu $GPU train --dataset BM --features_type ssl --ssl_pre_train encoder_fe --ed_training true
+python xr2learn_enablers_cli/xr2learn_enablers.py --experiment_id $EXPERIMENT_ID --config_file $PATH_JSON --gpu $GPU train --dataset Xroom --modality body-tracking --features_type none --ssl_pre_train none --ed_training true
